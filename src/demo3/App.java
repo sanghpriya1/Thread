a1a851812a8ae223e18b5bff45e994e5abf8e2f4
sanghpriya1 <sanghpriya2009@gmail.com>
2021-06-24 02:38:41 +0530

    Updated

---
+++ src/demo3/App.java
@@ -0,0 +1,30 @@
+package demo3;
+
+public class App {
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for(int i = 0; i < 10; i++) {
+					System.out.println("Hello " + i);
+					try {
+						Thread.sleep(100);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}
+			
+		}
+				
+				
+				);
+		t1.start();
+		
+
+	}
+
+}
